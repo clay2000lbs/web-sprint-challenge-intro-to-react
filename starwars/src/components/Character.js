88d0912dd2f1099ea4bbf5900cb4ebca5913e7ba
unknown <clay2000lbs@gmail.com>
2021-01-10 18:45:58 -0600

    hourly commit

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from "react";
+
+const Character = () => {
+
+
+
+
+    
+    return (
+        <div className ="characterList">
+
+        </div>
+    )
+}
+
+default export Character;
